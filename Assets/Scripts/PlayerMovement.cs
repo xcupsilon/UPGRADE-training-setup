4cb109c43ec4cce8c9920107f41c3005e64a6431
Xiaocong (Rain) Yan <50472308+xcupsilon@users.noreply.github.com>
2021-09-24 20:59:40 -0400

    part 3 finished: fixed collision box; player movement implemented

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,18 +1,26 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float moveSpeed = 5f;
+
+    private Vector2 movement;
+    
+    public Rigidbody2D rb;
+
+
+    private void Update()
     {
-        
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+
+        rb.MovePosition(rb.position + Time.deltaTime * movement * moveSpeed);
     }
 }
