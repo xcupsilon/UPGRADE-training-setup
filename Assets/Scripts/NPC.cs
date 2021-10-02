f2ff1d4d3582e25c8a6bedb9c717420b5c1507e6
Xiaocong (Rain) Yan <50472308+xcupsilon@users.noreply.github.com>
2021-10-02 17:03:15 -0400

    part 4 demo finished: yarn implemented with interactable npc

--- Assets/Scripts/NPC.cs
+++ Assets/Scripts/NPC.cs
@@ -1,12 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using Yarn.Unity;
 
 public class NPC : MonoBehaviour, IInteractable
 {
+    [SerializeField] private DialogueRunner runner;
+
     public bool Interacted(GameObject obj)
     {
         Debug.Log("NPC has been interacted by " + obj.name);
+
+        runner.StartDialogue();
         
         return true;
     }
